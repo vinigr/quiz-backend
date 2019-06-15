94ebc3f595fef28c7b38455bcb2c0c95f6b63efb
Vinícios Grama <vinyirun1@gmail.com>
2019-06-15 11:20:10 -0300

    Sequelize adicionado

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  dialect: 'postgres',
+};
