c1ebb6d18d37cec044f6142076976720fa5ddb7c
Vinícios Grama <vinyirun1@gmail.com>
2019-06-19 13:56:35 -0300

    Rotas para recuperação de senhas desenvolvidas

--- src/app/controllers/player.js
+++ src/app/controllers/player.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+const { Op } = require('sequelize');
+const addMinutes = require('date-fns/add_minutes');
 const { User, LocalAuth } = require('../models');
 const Helper = require('../helper');
 
@@ -55,7 +58,7 @@ const signUp = async (req, res) => {
 };
 
 
-const SignIn = async (req, res) => {
+const signIn = async (req, res) => {
   const { email, password } = req.body;
 
   const userLocal = await LocalAuth.findOne({ where: { email } });
@@ -71,19 +74,104 @@ const SignIn = async (req, res) => {
   if (!compareSenha) {
     return res
       .status(400)
-      .send({ message: 'Credenciais inválidas.' });
+      .send({ message: 'Invalid credentials' });
   }
 
   userLocal.password = undefined;
 
   const user = await User.findOne({ where: { local_auth: userLocal.id } });
-  // console.log(user);
 
   const token = Helper.generateToken(user.id);
   return res.status(201).send({ token });
 };
 
+
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    res.status(400)
+      .send({ message: 'Required email' });
+  }
+
+  const transport = nodemailer.createTransport({
+    host: process.env.MAILHOG_HOST,
+    port: '1025',
+    auth: null,
+  });
+
+  try {
+    const userLocal = await LocalAuth.findOne({ where: { email } });
+    const token = crypto.randomBytes(20).toString('hex');
+    await userLocal.update({
+      reset_passwordToken: token,
+      reset_passwordExpires: addMinutes(new Date(), 10),
+    });
+
+    transport.sendMail({
+      from: 'Eu mesmo <vinyirun4@hotmail.com>',
+      to: `${userLocal.name} <${userLocal.email}>`,
+      subject: 'Link to reset password',
+      text:
+        'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
+        + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
+        + `http://localhost:3000/resetPassword/${token}\n\n`
+        + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
+    });
+
+    return res.status(201).json(userLocal);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
+const resetPassword = async (req, res) => {
+  const { token } = req.params;
+  // const { Op } = Sequelize;
+  try {
+    const userLocal = await LocalAuth.findOne({
+      where: {
+        reset_passwordToken: token,
+        reset_passwordExpires: {
+          [Op.gt]: new Date(),
+        },
+      },
+    });
+
+    if (!userLocal) return res.status(403).send({ message: 'Password reset link is invalid or has expired' });
+
+    return res.status(201).json({ id: userLocal.id });
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
+const updatePassword = async (req, res) => {
+  const { id, password } = req.body;
+
+  try {
+    const userLocal = await LocalAuth.findOne({
+      where: { id },
+    });
+
+    if (!userLocal) return res.status(403).send({ message: 'User not found' });
+
+    const hashPassword = Helper.hashPassword(password);
+
+    await userLocal.update({
+      password: hashPassword,
+    });
+
+    return res.status(200).send(userLocal);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
 module.exports = {
   signUp,
-  SignIn,
+  signIn,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const player = require('./app/controllers/player');
 
 router
   .post('/player/signup', player.signUp)
-  .post('/player/signin', player.SignIn);
+  .post('/player/signin', player.signIn)
+  .put('/player/forgotPassword', player.forgotPassword)
+  .get('/resetPassword/:token', player.resetPassword)
+  .put('/player/updatePassword', player.updatePassword);
 
 module.exports = router;
